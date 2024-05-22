1b34bb55f33c6c296a3e5b54415f7b8acafad6ac
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2024-05-22 15:36:44 -0300

    atualizado import css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 // pages/_app.js
 import { CartProvider } from '../context/CartContext';
 import '../styles/globals.css';
+import '../styles/index.css';
+import '../styles/fullscreen-button.css';
+import '../styles/cart.css';
 
 function MyApp({ Component, pageProps }) {
   return (
