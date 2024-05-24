f42e5a2594af01d9f98a18742094b4234e09c48c
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2024-05-24 13:11:47 -0300

    corrigido erro de chamar função quando quantidade é 0

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import Image from 'next/image';
 
-const ProductCard = ({ product, getProductQuantity, addToCart, removeFromCart }) => (
-  <li className="product-card">
-    <Image src={product.image} alt={product.name} width={200} height={200} priority />
-    <div>
-      <h3>{product.name}</h3>
-      <span>
-        R$
-        {' '}
-        {product.price.toFixed(2)}
-      </span>
-    </div>
-    <div className="product-buttons">
-      {product.stock > 0 ? (
-        <>
-          <button type="button" className="remove-item-button" onClick={() => removeFromCart(product)}>-</button>
-          <span>{getProductQuantity(product)}</span>
-          <button type="button" className="add-item-button" onClick={() => addToCart(product)}>+</button>
-        </>
-      ) : (
-        <span>Item indisponível</span>
-      )}
-    </div>
-  </li>
-);
+const ProductCard = ({ product, getProductQuantity, addToCart, removeFromCart }) => {
+  const quantity = getProductQuantity(product);
+
+  return (
+    <li className="product-card">
+      <Image src={product.image} alt={product.name} width={200} height={200} priority />
+      <div>
+        <h3>{product.name}</h3>
+        <span>
+          R$
+          {' '}
+          {product.price.toFixed(2)}
+        </span>
+      </div>
+      <div className="product-buttons">
+        {product.stock > 0 ? (
+          <>
+            <button
+              type="button"
+              className="remove-item-button"
+              onClick={() => removeFromCart(product)}
+              disabled={quantity === 0} // Desabilitar o botão quando a quantidade for 0
+            >
+              -
+            </button>
+            <span>{quantity}</span>
+            <button type="button" className="add-item-button" onClick={() => addToCart(product)}>+</button>
+          </>
+        ) : (
+          <span>Item indisponível</span>
+        )}
+      </div>
+    </li>
+  );
+};
 
 export default ProductCard;
